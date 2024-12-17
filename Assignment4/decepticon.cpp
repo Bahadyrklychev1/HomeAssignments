7d2898f0f5bc1cf8eb974196bb770b6c51c05985
Bahadyrklychev1 <st135363@student.spbu.ru>
2024-12-17 09:50:47 +0000

    New 5

---
+++ Assignment4/decepticon.cpp
@@ -0,0 +1,17 @@
+//Клычев Баходир Исламбекович, группа 24.Б83-мм 
+//Implementation of Transformer classes
+#include "decepticon.h"
+#include <iostream>
+
+bool Decepticon::sabotage() {
+    std::cout << name << " is sabotaging!" << std::endl;
+    return true;
+}
+
+void Decepticon::transform() {
+    std::cout << name << " is transforming!" << std::endl;
+}
+
+void Decepticon::useWeapon() const {
+    weapon.use();
+}
