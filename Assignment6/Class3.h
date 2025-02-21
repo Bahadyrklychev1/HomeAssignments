1c262a00768a44bc605e2e218a27a45009d76363
Bahadyrklychev1 <st135363@student.spbu.ru>
2025-02-21 19:35:06 +0000

    Add Class3.h

--- Assignment6/Class3.h
+++ Assignment6/Class3.h
@@ -1,4 +1,3 @@
-
 #ifndef CLASS3_H
 #define CLASS3_H
 
@@ -6,10 +5,10 @@
 
 class Class3 {
 public:
-    static bool bar(int number, const std::vector<float>& vector);
-    static int c_3_1();
-    static float c_3_2();
-    static void c_3_3();
+    bool bar(int number, const std::vector<float>& vector);
+    int method_1();
+    float method_2();
+    void method_3();
 };
 
-#endif
+#endif // CLASS3_H
