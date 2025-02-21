be1d007ae361909a273f5f9652dfcf0fe6f6fae9
Bahadyrklychev1 <st135363@student.spbu.ru>
2025-02-21 19:33:34 +0000

    Add Class2.h

--- Assignment6/Class2.h
+++ Assignment6/Class2.h
@@ -1,4 +1,3 @@
-
 #ifndef CLASS2_H
 #define CLASS2_H
 
@@ -6,10 +5,10 @@
 
 class Class2 {
 public:
-    static bool bar(int number, const std::vector<float>& vec);
-    static int c_2_1();
-    static float c_2_2();
-    static void c_2_3();
+    bool bar(int number, const std::vector<float>& vector);
+    int method_1();
+    float method_2();
+    void method_3();
 };
 
-#endif
+#endif // CLASS2_H
