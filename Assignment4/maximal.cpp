f1e50b9c1fb6377d6bafe04f86d1e814eb973c93
NewProject1112321 <bahadyrklychev@gmail.com>
2025-03-20 17:31:58 +0000

    Удалены ненужные файлы

--- Assignment4/maximal.cpp
+++
@@ -1,8 +0,0 @@
-//Клычев Баходир Исламбекович, группа 24.Б83-мм 
-//Implementation of Transformer classes
-#include "maximal.h"
-#include <iostream>
-
-void Maximal::transform() {
-    std::cout << name << " is transforming!" << std::endl;
-}
